f480dd264221844b11378b9cecfaa1e53de9e3f7
stevenyangh <stevenyanmu@outlook.com>
2019-03-19 20:26:19 +0800

    Ready to give out

---
+++ PrimalProcess.py
@@ -0,0 +1,17 @@
+# def scope_of_mark_sep(prevScope):
+
+
+# def scope_of_mark_pair():
+
+def is_paragraph_mark(word):
+    for character in word:
+        if character == '\n':
+            return True
+    return False
+
+
+def is_sentence_mark(word):
+    for character in word:
+        if character == '.':
+            return True
+    return False
--- TextInsight.py
+++ TextInsight.py
@@ -1,20 +1,39 @@
-import PrimalProcess
-
-
 class Sentence_Node:
-    def __init__(self):
-        self = {"name" : "", "children" : []}
+    def __init__(self,):
+        self = {"text": [], "type": "sentence", "children": []}
 
 
-def makeHierarchyAccordForm(word_list_gen, root_trace, level_method_gen):
-    roots = []
-    for word in word_list_gen:
-        if PrimalProcess.is_paragraph_mark():
-            yield
+def is_sentence_mark(word):
+    for character in word:
+        if character == '.':
+            return True
+    return False
 
+
+def is_paragraph_mark(word):
+    for character in word:
+        if character == '\n':
+            return True
+    return False
+
+
+def cutSentence(word_list):
+    sent = Sentence_Node()
+    for word in word_list:
+        sent["text"].append(word)
+        if is_sentence_mark(word):
+            yield sent
+# def makeHierarchyAccordForm(word_list_gen, root_trace, level_method_gen):
+
+
+def makeWordVector(text):
+    wordSet = set(text)
+    wordList = []
+    for index in range(len(wordSet))
+        wordList.append(pop(wordSet))
+    for word in text
 
 def generateGraph():
-
     yield 0
 
 
